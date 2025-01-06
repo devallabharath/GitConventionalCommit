1f6ecb40dcfc39481656340509068b4fee161a22
Bharath <devallabharath619@gmail.com>
2025-01-05 22:44:07 -0500

    Init: ✨ Project initialization.

---
+++ GitConventionalCommit/GitConventionalCommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitConventionalCommitApp.swift
+//  GitConventionalCommit
+//
+//  Created by Devalla Bharath on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitConventionalCommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
